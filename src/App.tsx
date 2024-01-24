a70d847d1076f0aae227eda171b0d5766d3f498b
Mateus Villa Nova <villa1999nova@gmail.com>
2024-01-24 09:48:17 -0300

    feat: Wrapping routes with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
+
 import { Router } from './Router'
+import { CyclesContextProvider } from './context/CyclesContext'
 
 export function App() {
   return (
     <>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
     </>
   )
